f7e4a66e0a79fcead5a4dcc0cd807738ecea29b9
Andre Harris <andreharris@Andres-MacBook-Pro-2-2018.local>
2019-06-13 13:15:29 +0200

    Basic Web Folder

---
+++ Basic_Web/Basic_Web.js
@@ -0,0 +1,30 @@
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
+
+/*IMAGE GALLERY CODE ENDS!!!*/
+
+
+/*NAVIGATION BAR CODE BEGINS*/
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+
+/* END OF NAVIGATION CODE!!!*/
